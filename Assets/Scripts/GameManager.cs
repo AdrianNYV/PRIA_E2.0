3e7ff764927af72960525c3589de821596b3c0aa
Adrián Nicolás Yáñez Viceiro <yanvicadr@IESPIRINGALLA.AD>
2023-05-23 18:37:43 +0200

    Añadido movimiento del Player

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,11 @@
+using Unity.Netcode;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using Unity.Netcode;
 
-public class GameManager : MonoBehaviour {
-    void OnGUI() {
+namespace HelloWorld {
+    public class GameManager : MonoBehaviour {
+        void OnGUI() {
             GUILayout.BeginArea(new Rect(10, 10, 300, 300));
             if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer) {
                 StartButtons();
@@ -18,11 +19,13 @@ public class GameManager : MonoBehaviour {
             if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
             if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
             if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+            
         }
 
         void StatusLabels() {
             var mode = NetworkManager.Singleton.IsHost ? "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
             GUILayout.Label("Transport: " + NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
             GUILayout.Label("Mode: " + mode);
-        }    
+        }
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 using System;
 using Unity.Netcode;
 
-public class Player : MonoBehaviour {
-    public float speed = 5f;
+public class Player : NetworkBehaviour {
+    public float speed = 4f;
 
-    private MeshRenderer meshRenderer;
+    private MeshRenderer meshRenderer; 
 
     void Awake() {
         meshRenderer = GetComponent<MeshRenderer>();
     }
 
+    [ServerRpc]
+    void MoveServerRpc(Vector3 dir) {
+        transform.position += dir * speed * Time.deltaTime;
+    }
+
     void Update() {
-        
+        if(IsOwner) {
+            if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+                MoveServerRpc(Vector3.right);
+            }
+            if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+                MoveServerRpc(Vector3.left);
+            }
+            if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+                MoveServerRpc(Vector3.forward);
+            }
+            if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+                MoveServerRpc(Vector3.back);
+            }
+        }
     }
 }
